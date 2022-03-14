be2c53452c72e9ee89e668da2276246301ff7983
shakederin <89574610+shakederin@users.noreply.github.com>
2022-03-14 23:16:13 +0200

    add cursor drag

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TornadoIcon from '@mui/icons-material/Tornado';
-
+import interact from 'interactjs'
+import { querySelectorAll } from 'dom-helpers';
 
 export default function Cursor(){
     const [time, setTime] = useState(0); 
@@ -11,10 +12,33 @@ export default function Cursor(){
             setTime((allAudio[0].currentTime / allAudio[0].duration) * 100);
           });
     },[])
-   
+
+    const cursor = interact('.line');
+    cursor.draggable({
+        origin: "self",
+        inertia: false,
+        modifiers: [
+          interact.modifiers.restrictRect({
+            restriction: "parent"
+          }),
+        ],
+        listeners: {
+          move(event) {
+            let value = event.pageX;
+            value = (value > 100 ? value = 100 : event.pageX)
+            value = (value < 0 ? value = 0 : event.pageX)
+            const audioElements = document.querySelectorAll(".track")
+            audioElements.forEach((audioElement) => {
+                audioElement.currentTime =
+                (audioElements[0].duration * Math.floor(value)) / 100;
+            });
+            setTime(value);
+          },
+        },
+      });
 
     return (
-        <div className="line" style={{left:`${time}%`}}>
+        <div className="line" style={{left:`${time}%`, cursor: "grab"}}>
             <TornadoIcon/>
         </div>
   )
